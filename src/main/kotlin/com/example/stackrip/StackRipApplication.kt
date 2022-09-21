d36064d1ffe05d4a76ab1b3f5cbfadde65b632e8
Amit-Sheer Cohen <amitcohen95@gmail.com>
2022-09-21 19:35:22 +0300

    init

---
+++ src/main/kotlin/com/example/stackrip/StackRipApplication.kt
@@ -0,0 +1,11 @@
+package com.example.stackrip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StackRipApplication
+
+fun main(args: Array<String>) {
+    runApplication<StackRipApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/stackrip/StackRipApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.stackrip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StackRipApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
